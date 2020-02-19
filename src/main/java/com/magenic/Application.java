7988fa536070333ef558c1622930c9568f9beab8
Stuart Blavatnik <stuartb@magenic.com>
2020-02-19 08:55:21 -0500

    Added comment

--- src/main/java/com/magenic/Application.java
+++ src/main/java/com/magenic/Application.java
@@ -3,6 +3,8 @@ package com.magenic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Added comment
+
 @SpringBootApplication
 public class Application {
 
